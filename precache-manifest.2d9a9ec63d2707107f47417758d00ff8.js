8ca34d41fa69807b97477b5b0347fbaf96962ec3
haseeb124 <haseebz124@gmail.com>
2022-09-08 05:23:54 +0500

    Updates

---
+++ precache-manifest.2d9a9ec63d2707107f47417758d00ff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eed732ced7d8d5ed1b5d",
+    "url": "/TextUtility/static/css/main.9e9ceacc.chunk.css"
+  },
+  {
+    "revision": "eed732ced7d8d5ed1b5d",
+    "url": "/TextUtility/static/js/main.eed732ce.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/TextUtility/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtility/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "9bafe2f51af8289d10a4",
+    "url": "/TextUtility/static/js/runtime~main.9bafe2f5.js"
+  },
+  {
+    "revision": "5a5bf97e57581f233293e4f0f4110bc6",
+    "url": "/TextUtility/index.html"
+  }
+];
